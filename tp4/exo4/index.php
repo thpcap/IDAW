cf08121b7d1eda458b9a23a452a175e40cbf9023
thpcap <126089066+thpcap@users.noreply.github.com>
2024-10-14 14:10:58 +0200

    bewtified and added ubdate in tp4

--- tp4/exo4/index.php
+++ tp4/exo4/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Hello World</title>
+        <title>Users Admin CRUD</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <form method="POST">
+        <h1>Users Database CRUD</h1>
+        <form method="POST" id="Delete">
             <input name="Delete" type="hidden">
             <table class="tableDb">
                 <?php
@@ -47,7 +48,7 @@
                 ?>
                 <thead>
                     <tr>
-                        <th scope="col"  colspan="4"><h1 style="margin: 0;">Table: Users</h1></th>
+                        <th scope="col"  colspan="4"><h2 style="margin: 0;">Table: Users</h2></th>
                     </tr>
                     <tr>    
                         <th scope="col"><input type="submit" value="Delete"></th>
@@ -71,27 +72,59 @@
             </table>
         </form>
         <br>
-        <form method="POST">
-            <input name="Add" type="hidden">
+        <form method="POST" id="Create">
+            <input name="Create" type="hidden" >
             <table class="tableform">
                 <tr>
+                    <th colspan="2"><h2 style="margin: 0;">Create a User</h2></th>
+                </tr>
+                <tr>
                     <th>Name:</th>
-                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name" autocomplete="off"></td>
                 </tr>
                 <tr>
                     <th>Email:</th>
-                    <td><input type="email" name="email"></td>
+                    <td><input type="email" name="email" autocomplete="off"></td>
                 </tr>
                 <tr>
-                    <td><input type="submit" value="Add"></td>
+                    <td><input type="submit" value="Create"></td>
                     <td><input type="reset" value="Reset"></td>
+                </tr>
+            </table>
+        </form>
+        <br>
+        <form method="POST" id="Update">
+            <input name="Update" type="hidden" >
+            <table class="tableform">
+                <tr>
+                    <th colspan="2"><h2 style="margin: 0;">Update a User</h2></th>
+                </tr>
+                <tr>
+                    <th>Id:</th>
+                    <td><input type="number" name="id" autocomplete="off"></td>
+                </tr>
+                <tr>
+                    <th>Name:</th>
+                    <td><input type="text" name="name" autocomplete="off"></td>
+                </tr>
+                <tr>
+                    <th>Email:</th>
+                    <td><input type="email" name="email" autocomplete="off"></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Update"></td>
+                    <td><input type="reset" value="Reset"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><p style="font-size: x-small; margin:0;">N.B.The User modified will be the user with the provided Id</p></td>
                 </tr>
             </table>
         </form>
         
         <?php
             //gestion des requests
-            if(isset($_POST["Add"])&&isset($_POST["name"])&&isset($_POST["email"])&&$_POST['name']!=""&&$_POST['email']!=""){
+            //requete post pour ajouter un User
+            if(isset($_POST["Create"])&&isset($_POST["name"])&&isset($_POST["email"])&&$_POST['name']!=""&&$_POST['email']!=""){
                 $name=$_POST["name"];
                 $email=$_POST["email"];
                 require_once('config.php');
@@ -113,6 +146,7 @@
                 $pdo=null;
                 header( "refresh:0" );
             }
+            //requete post pour supprimer un User
             if(isset($_POST["Delete"])&&isset($_POST["id"])){
                 $id=$_POST["id"];
                 
@@ -132,6 +166,32 @@
                     echo 'Erreur : '.$erreur->getMessage();
                 }
                 $querry="DELETE FROM `users` WHERE `id`=".$id;
+                $pdo->query($querry);
+                $pdo=null;
+                header( "refresh:0" );
+            }
+            //requete post pour modifier un User
+            if(isset($_POST["Update"])&&isset($_POST["id"])){
+                $id=$_POST["id"];
+                $name=$_POST["name"];
+                $email=$_POST["email"];
+                
+                //TODO ajouter dans la base les données
+                require_once('config.php');
+                $connectionString = "mysql:host=". _MYSQL_HOST;
+                if(defined('_MYSQL_PORT'))
+                    $connectionString .= ";port=". _MYSQL_PORT;
+                $connectionString .= ";dbname=" . _MYSQL_DBNAME;
+                $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+                $pdo = NULL;
+                try {
+                    $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                }
+                catch (PDOException $erreur) {
+                    echo 'Erreur : '.$erreur->getMessage();
+                }
+                $querry="UPDATE `users` SET `id`=".$id.", `name` = '".$name."', `email` ='".$email."' WHERE `users`.`id` =".$id;
                 $pdo->query($querry);
                 $pdo=null;
                 header( "refresh:0" );
